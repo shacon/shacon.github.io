05a323da2c11def3080bd44391e0c178baa8e207
shacon <shannonjc143@gmail.com>
2021-12-05 12:29:17 -0600

    changes

---
+++ relax.js
@@ -0,0 +1,34 @@
+
+var colorChoices = ["red", "orangered", "gold", "limegreen", "dodgerblue", "slateblue", "rebeccapurple"]
+
+var currentColor = colorChoices[0];
+
+var divItems = document.getElementsByClassName("shape");
+
+function clear() {
+  for(var i=0; i < divItems.length; i++) {
+    var item = divItems[i];
+    item.style.backgroundColor = 'white';
+  }
+}
+
+function getNextColor(currentColor) {
+	currentIndex = this.colorChoices.indexOf(currentColor);
+	if (currentIndex < (colorChoices.length - 1)) {
+		return colorChoices[currentIndex + 1]
+	} else {
+		return colorChoices[0]
+	}
+}
+
+function changeColor(item) {
+  console.log('changed color')
+  var currentColor = item.style.backgroundColor;
+  if (colorChoices.includes(currentColor)) {  	
+  	item.style.backgroundColor = "peachpuff"
+  	this.currentColor = this.colorChoices[0]
+  } else {
+  	item.style.backgroundColor = this.currentColor;
+  	this.currentColor = getNextColor(this.currentColor)
+  }
+}
---
+++ test_src.js
@@ -0,0 +1,53 @@
+var showLives = document.getElementById("mylives");
+var button1 = document.getElementById("myButton");
+var count = document.getElementById("myCount");
+var showLetter = document.getElementById("letterSelected");
+var counted = 0;
+var addButton = document.getElementById("add");
+var subtractButton = document.getElementById("subtract");
+var currentNumber = 0;
+var countDiv = document.getElementById("countDiv");
+var colorChangeDiv = document.getElementById("changeColor");
+
+// incrementCount = function () {
+//   counted ++;
+//   count.innerHTML = `The new count is ${counted}`
+// }
+
+// function processButtonClick(letter) {
+// 	showLives.innerHTML = 'This is from the javascript file!'
+// 	showLetter.innerHTML = letter;
+// 	console.log('here')
+// 	console.log(letter)
+// 	incrementCount();
+// }
+
+// button1.onclick = function (event) {
+//   processButtonClick(event)
+// }
+
+
+// addButton.onclick = function (event) {
+// 	currentNumber = currentNumber + 1;
+// 	countDiv.innerHTML = currentNumber;
+// }
+
+// subtractButton.onclick = function (event) {
+// 	currentNumber = currentNumber - 1;
+// 	countDiv.innerHTML = currentNumber;
+// }
+
+var colorState = "original"
+
+colorChangeDiv.onclick = function (event) {
+	console.log(colorChangeDiv.style.backgroundColor)
+	if (colorState === "changed") {
+		colorState = "original"
+		colorChangeDiv.style.backgroundColor = "peachpuff"
+	} else {
+		colorState = "changed"
+		colorChangeDiv.style.backgroundColor = "#ade1bb";
+	}
+	
+}
+
